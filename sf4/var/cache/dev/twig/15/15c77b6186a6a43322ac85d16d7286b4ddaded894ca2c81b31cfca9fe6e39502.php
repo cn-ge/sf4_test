b7f9bb2c073f94ec4ae8664b2d9f808b4902aa28
cn-ge <cgeindreau@Mac-mini-de-WEMOOV.local>
2019-09-30 12:25:54 +0200

    correction mapping src docker-compose.yml, restructuration des fichiers

---
+++ sf4/var/cache/dev/twig/15/15c77b6186a6a43322ac85d16d7286b4ddaded894ca2c81b31cfca9fe6e39502.php
@@ -0,0 +1,82 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @Framework/FormTable/form_widget_compound.html.php */
+class __TwigTemplate_b8e2bf5bff5ccb4e9d7f445d069483b15d75a62ad0ae8a4c46f7eedf25dd174b extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Framework/FormTable/form_widget_compound.html.php"));
+
+        // line 1
+        echo "<table <?php echo \$view['form']->block(\$form, 'widget_container_attributes'); ?>>
+    <?php if (!\$form->parent && \$errors): ?>
+    <tr>
+        <td colspan=\"2\">
+            <?php echo \$view['form']->errors(\$form); ?>
+        </td>
+    </tr>
+    <?php endif; ?>
+    <?php echo \$view['form']->block(\$form, 'form_rows'); ?>
+    <?php echo \$view['form']->rest(\$form); ?>
+</table>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Framework/FormTable/form_widget_compound.html.php";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<table <?php echo \$view['form']->block(\$form, 'widget_container_attributes'); ?>>
+    <?php if (!\$form->parent && \$errors): ?>
+    <tr>
+        <td colspan=\"2\">
+            <?php echo \$view['form']->errors(\$form); ?>
+        </td>
+    </tr>
+    <?php endif; ?>
+    <?php echo \$view['form']->block(\$form, 'form_rows'); ?>
+    <?php echo \$view['form']->rest(\$form); ?>
+</table>
+", "@Framework/FormTable/form_widget_compound.html.php", "/Users/cgeindreau/Documents/projects/sf4_test/sf4/vendor/symfony/framework-bundle/Resources/views/FormTable/form_widget_compound.html.php");
+    }
+}
---
+++ sf4/var/cache/dev/twig/a3/a387e8fae258310a6da397d49c33648c349933e7bb0f7732153c5aaf2574659a.php
@@ -0,0 +1,62 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @Framework/Form/textarea_widget.html.php */
+class __TwigTemplate_084a3938a7fe2311e1c6c4b1aebfac43df65463b036b1fd4a43b26d7f562725f extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Framework/Form/textarea_widget.html.php"));
+
+        // line 1
+        echo "<textarea <?php echo \$view['form']->block(\$form, 'widget_attributes') ?>><?php echo \$view->escape(\$value) ?></textarea>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Framework/Form/textarea_widget.html.php";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<textarea <?php echo \$view['form']->block(\$form, 'widget_attributes') ?>><?php echo \$view->escape(\$value) ?></textarea>
+", "@Framework/Form/textarea_widget.html.php", "/Users/cgeindreau/Documents/projects/sf4_test/sf4/vendor/symfony/framework-bundle/Resources/views/Form/textarea_widget.html.php");
+    }
+}
