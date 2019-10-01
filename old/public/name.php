bb96cb311fb1498de482268c016264c2310ae27d
cn-ge <cgeindreau@Mac-mini-de-WEMOOV.local>
2019-10-01 19:00:48 +0200

    ajout du mapping dans docker-compose.yml

--- old/public/name.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// en utilisant l'url localhost?name=Zozor
-$name = $_GET['name'];
-
-header('Content-Type: text/html');
-echo '<html>';
-echo '<body>Bonjour '. $name . '</body>';
-echo '</html>';
